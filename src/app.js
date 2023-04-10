119346ffc8a6ef0dbe7081400980a5c12d4aa165
LordAkirast <darkwarish@gmail.com>
2023-04-10 08:37:01 -0300

    Tweets now shall return [] if lenght is 0.

--- src/app.js
+++ src/app.js
@@ -9,50 +9,54 @@ const users = []; // lista de usuários cadastrados
 
 app.post('/sign-up', (req, res) => {
     const { username, avatar } = req.body;
-  
+
     if (!username || !avatar) {
-      res.status(400).send({ error: 'Campos obrigatórios não preenchidos' });
-      return;
+        res.status(400).send({ error: 'Campos obrigatórios não preenchidos' });
+        return;
     }
-  
+
     // verifica se o usuário já está cadastrado
     const isUserRegistered = users.some(user => user.username === username);
-  
+
     if (isUserRegistered) {
-      res.status(409).send({ error: 'Usuário já cadastrado' });
-      return;
+        res.status(409).send({ error: 'Usuário já cadastrado' });
+        return;
     }
-  
+
     // adiciona o usuário à lista de usuários cadastrados
     users.push({ username, avatar });
-    
+
     res.status(200).send('OK');
-  });
-  
+});
+
 
 const tweets = []; // lista de tweets
 
 app.post('/tweets', (req, res) => {
-  const { username, tweet } = req.body;
+    const { username, tweet } = req.body;
 
-  // verifica se o usuário está cadastrado
-  const isUserRegistered = users.some(user => user.username === username);
+    // verifica se o usuário está cadastrado
+    const isUserRegistered = users.some(user => user.username === username);
 
-  if (!isUserRegistered) {
-    res.status(401).send({ error: 'Usuário não autorizado' });
-    return;
-  }
+    if (!isUserRegistered) {
+        res.status(401).send({ error: 'Usuário não autorizado' });
+        return;
+    }
 
-  // adiciona o tweet à lista de tweets
-  tweets.push({ username, tweet });
+    // adiciona o tweet à lista de tweets
+    tweets.push({ username, tweet });
 
-  res.send({ message: 'OK' });
+    res.send({ message: 'OK' });
 });
 
 app.get('/tweets', (req, res) => {
-  res.send({ tweets });
+    if (tweets.length === 0) {
+        res.send([])
+    } else {
+        res.send({ tweets });
+    }
 });
 
 app.listen(5000, () => {
-  console.log('Servidor iniciado na porta 5000');
+    console.log('Servidor iniciado na porta 5000');
 });
